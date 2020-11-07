4b1daeee52966ef67ab36ebf940a1cae634bfc0e
iexpurgator <arielwm351@gmail.com>
2020-11-07 15:46:57 +0700

    update main

--- asm_chall/asm_chall/main.asm
+++ asm_chall/asm_chall/main.asm
@@ -1,11 +1,18 @@
 INCLUDE Irvine32.inc
 INCLUDE Macros.inc
-
+; uppercase
 .DATA
-
+	s BYTE ?
 .CODE
 main PROC
-	mWrite <"Hello World!", 13, 10, 0>
+	mov edx, offset s
+	mov ecx, 33
+	CALL ReadString
+	mov ecx, eax
+	mov edi, 
+	L1:
+		;
+	LOOP L1
 	push 0
 	call ExitProcess
 main ENDP
